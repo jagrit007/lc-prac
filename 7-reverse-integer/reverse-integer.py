72b68be41d9259a12096c3c7d9f7ad8fe441b30e
Jagrit Thapar <jagritthapar007@gmail.com>
2024-02-22 21:30:33 +0530

    Time: 36 ms (65.42%) | Memory: 16.6 MB (81.63%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,9 +1,10 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        MAX_VAL = 2**31
         rev = 0
         isNeg = True if x < 0 else False
         x = abs(x)
         while x != 0:
             rev = (rev * 10) + (x%10)
             x = floor(x/10)
-        return -rev if isNeg and -rev > -(2**31) else (0 if rev > (2**31) else rev)
+        return -rev if isNeg and -rev > -(MAX_VAL) else (0 if rev > (MAX_VAL) else rev)
