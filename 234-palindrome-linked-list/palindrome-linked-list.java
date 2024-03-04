b42593824aa734174eba13dd6b707f8e81c6dd36
Jagrit Thapar <jagritthapar007@gmail.com>
2024-03-04 16:16:17 +0530

    Time: 9 ms (25.67%) | Memory: 71.5 MB (5.66%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,7 +9,29 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
+
+    public static ListNode recursiveReverse(ListNode head){
+        if(head == null || head.next == null){return head;}
+
+        ListNode new_head = recursiveReverse(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return new_head;
+    }
+
+    public ListNode findMiddle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean isPalidrome2(ListNode head){
         ArrayList<Integer> list = new ArrayList<Integer>();
         ListNode temp = head;
 
@@ -28,5 +50,23 @@ class Solution {
             j--;
         }
         return true;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){return true;}
+
+        ListNode middle = findMiddle(head);
+        ListNode revHead = recursiveReverse(middle.next);
+        
+        ListNode firstHead = head;
+
+        while(revHead != null){
+            if(firstHead.val != revHead.val){
+                return false;
+            }
+            firstHead = firstHead.next;
+            revHead = revHead.next;
+        }
+        return true;
+
     }
 }
