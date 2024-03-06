dfbce8853dfb3148d7bf2b73ef059bb44a5b3343
Jagrit Thapar <jagritthapar007@gmail.com>
2024-03-06 19:37:50 +0530

    Time: 417 ms (44.28%) | Memory: 17.3 MB (53.54%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -7,8 +7,8 @@ class Solution:
         for row in range(rows):
             for col in range(cols):
                 total, count = 0, 0
-                for i in range(row - 1, row + 1+1):
-                    for j in range(col- 1, col + 1 +1):
+                for i in range(row - 1, row + 2):
+                    for j in range(col- 1, col + 2):
                         if i < 0 or i == rows or j < 0 or j == cols:
                             continue
                         total += img[i][j]
