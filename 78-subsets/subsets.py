6803d17937d99a841c2a5d623804cf40a5cd5385
Jagrit Thapar <jagritthapar007@gmail.com>
2024-03-11 13:46:29 +0530

    Time: 35 ms (73.80%) | Memory: 16.7 MB (81.60%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -5,7 +5,7 @@ class Solution:
         def helper(arr, i):
             result.append(arr[:])
             for j in range(i, len(nums)):
-                helper(arr + [nums[j]], j + 1)
-        
+                helper(arr + [nums[j]], j+1)
+
         helper([], 0)
         return result
