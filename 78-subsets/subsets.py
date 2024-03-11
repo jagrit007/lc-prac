a96add1a03f418077c3f563acbe5bfb14989bb18
Jagrit Thapar <jagritthapar007@gmail.com>
2024-03-11 13:44:56 +0530

    Time: 43 ms (19.21%) | Memory: 16.7 MB (81.60%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        
+        def helper(arr, i):
+            result.append(arr[:])
+            for j in range(i, len(nums)):
+                helper(arr + [nums[j]], j + 1)
+        
+        helper([], 0)
+        return result
